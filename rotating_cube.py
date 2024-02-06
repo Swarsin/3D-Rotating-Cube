d3916c5bc8c928151517f20df9024f7c9e86289f
Swarsin <124310642+Swarsin@users.noreply.github.com>
2024-02-06 00:17:46 +0000

    Update rotating_cube.py

--- rotating_cube.py
+++ rotating_cube.py
@@ -1,92 +1,48 @@
-# import pygame
-
-# pygame.init()
-
-# screen = pygame.display.set_mode((800, 800))
-# clock = pygame.time.clock()
-
-
-# cube_points = [
-#     [[-1], [-1], [1]],
-#     [[1], [-1], [1]],
-#     [[1], [1], [1]],
-#     [[-1], [1], [1]],
-#     [[-1], [-1], [-1]],
-#     [[1], [-1], [-1]],
-#     [[1], [1], [-1]],
-#     [[-1], [1], [-1]],
-# ]
-
-# projection 
-# def multiply_matrices(x, y):
-#     rows_x = len(x)
-#     cols_x = len(x[0])
-
-#     rows_y = len(y)
-#     cols_y = len(y[0])
-
-#     dot_product = sum([sum(x * y for x, y in zip(rows_x, cols_y)) for rows_x, cols_y in zip(x, zip(*y))])
-
-#     if cols_x == rows_y:
-#         for i in range(rows_x):
-#             for j in range(cols_y):
-#                 for k in range(rows_y):
-#                     dot_product[i][j] += x[i][k] * y[k][j]
-#     else:
-#         print("Can't do matrix multiplication - incompatible sizes")
-#     return dot_product
-
-# while True:
-#     clock.tick(60)
-#     for point in cube_points:
-#         point_2d = multiply_matrices()
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             pygame.quit()
-
-#     pygame.display.update()
-
-
-import pygame
+import pygame, numpy, sys
 from math import *
+
 
 WINDOW_SIZE =  800
 ROTATE_SPEED = 0.02
 window = pygame.display.set_mode( (WINDOW_SIZE, WINDOW_SIZE) )
 clock = pygame.time.Clock()
 
-projection_matrix = [[1,0,0],
-                     [0,1,0],
-                     [0,0,0]]
+projection_matrix = [
+    [1,0,0],
+    [0,1,0],
+    [0,0,0]
+    ]
 
-cube_points = [n for n in range(8)]
-cube_points[0] = [[-1], [-1], [1]]
-cube_points[1] = [[1],[-1],[1]]
-cube_points[2] = [[1],[1],[1]]
-cube_points[3] = [[-1],[1],[1]]
-cube_points[4] = [[-1],[-1],[-1]]
-cube_points[5] = [[1],[-1],[-1]]
-cube_points[6] = [[1],[1],[-1]]
-cube_points[7] = [[-1],[1],[-1]]
+cube_points = [
+    [[-1], [-1], [1]],
+    [[1], [-1], [1]],
+    [[1], [1], [1]],
+    [[-1], [1], [1]],
+    [[-1], [-1], [-1]],
+    [[1], [-1], [-1]],
+    [[1], [1], [-1]],
+    [[-1], [1], [-1]],
+    ]
 
 
-def multiply_m(a, b):
-    a_rows = len(a)
-    a_cols = len(a[0])
+# function that multiplies 2 matrices (no libraries, from scratch), but inefficient compared to numpy's matmul()
+# def multiply_m(a, b):
+#     a_rows = len(a)
+#     a_cols = len(a[0])
 
-    b_rows = len(b)
-    b_cols = len(b[0])
-    # Dot product matrix dimentions = a_rows x b_cols
-    product = [[0 for _ in range(b_cols)] for _ in range(a_rows)]
+#     b_rows = len(b)
+#     b_cols = len(b[0])
+#     # zero matrix with dimentions a_rows x b_cols:
+#     product = [[0 for _ in range(b_cols)] for _ in range(a_rows)]
 
-    if a_cols == b_rows:
-        for i in range(a_rows):
-            for j in range(b_cols):
-                for k in range(b_rows):
-                    product[i][j] += a[i][k] * b[k][j]
-    else:
-        print("INCOMPATIBLE MATRIX SIZES")
-    return product        
+#     if a_cols == b_rows:
+#         for i in range(a_rows):
+#             for j in range(b_cols):
+#                 for k in range(b_rows):
+#                     product[i][j] += a[i][k] * b[k][j]
+#     else:
+#         print("INCOMPATIBLE MATRIX SIZES")
+#     return product        
 
 
 def connect_points(i, j, points):
@@ -96,32 +52,34 @@ def connect_points(i, j, points):
 scale = 100
 angle_x = angle_y = angle_z = 0
 while True:
-    clock.tick(60)
+    clock.tick(30)
     window.fill((0,0,0))
     rotation_x = [[1, 0, 0],
-                    [0, cos(angle_x), -sin(angle_x)],
-                    [0, sin(angle_x), cos(angle_x)]]
+                  [0, cos(angle_x), -sin(angle_x)],
+                  [0, sin(angle_x), cos(angle_x)]
+                 ]
 
     rotation_y = [[cos(angle_y), 0, sin(angle_y)],
-                    [0, 1, 0],
-                    [-sin(angle_y), 0, cos(angle_y)]]
+                  [0, 1, 0],
+                  [-sin(angle_y), 0, cos(angle_y)]
+                 ]
 
     rotation_z = [[cos(angle_z), -sin(angle_z), 0],
-                    [sin(angle_z), cos(angle_z), 0],
-                    [0, 0, 1]]
+                  [sin(angle_z), cos(angle_z), 0],
+                  [0, 0, 1]
+                 ]
 
-  angle_x += 0.02
-  angle_y += 0.02
-  angle_z += 0.02
-
+    angle_x += 0.02
+    angle_y += 0.02
+    angle_z += 0.02
 
     points = [0 for _ in range(len(cube_points))]
     i = 0
     for point in cube_points:
-        rotate_x = multiply_m(rotation_x, point)
-        rotate_y = multiply_m(rotation_y, rotate_x)
-        rotate_z = multiply_m(rotation_z, rotate_y)
-        point_2d = multiply_m(projection_matrix, rotate_z)
+        rotate_x = numpy.matmul(rotation_x, point) #multiply_m(rotation_x, point)
+        rotate_y = numpy.matmul(rotation_y, rotate_x) #multiply_m(rotation_y, rotate_x)
+        rotate_z = numpy.matmul(rotation_z, rotate_y) #multiply_m(rotation_z, rotate_y)
+        point_2d = numpy.matmul(projection_matrix, rotate_z) #multiply_m(projection_matrix, rotate_z)
     
         x = (point_2d[0][0] * scale) + WINDOW_SIZE/2
         y = (point_2d[1][0] * scale) + WINDOW_SIZE/2
@@ -146,21 +104,6 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-        
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_r]:
-            angle_y = angle_x = angle_z = 0
-        if keys[pygame.K_a]:
-            angle_y += ROTATE_SPEED
-        if keys[pygame.K_d]:
-            angle_y -= ROTATE_SPEED      
-        if keys[pygame.K_w]:
-            angle_x += ROTATE_SPEED
-        if keys[pygame.K_s]:
-            angle_x -= ROTATE_SPEED
-        if keys[pygame.K_q]:
-            angle_z -= ROTATE_SPEED
-        if keys[pygame.K_e]:
-            angle_z += ROTATE_SPEED      
+            sys.exit()   
           
     pygame.display.update()
